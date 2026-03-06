6e275000e13ee68e9593b3cf3eef2925553cd8d6
Sumithreddy <160596444+SSumithreddy@users.noreply.github.com>
2026-03-06 22:12:12 +0530

    Add files via upload

---
+++ Day_2.py
@@ -0,0 +1,39 @@
+Student_Name = 'Sanivarapu Sumithreddy'
+Student_ID = 'CGH2948'
+Batch_No = 'PFS-HYD-050'
+Email ID = 'sanivarapusumithreddy@gmail.com'
+Date of Birth = '2003-03-30'
+Age = 22
+Gender = Male
+Blood Group = O+
+City = 'Narasaraopet'
+State = 'Andhra Pradesh'
+Student_no = '+918143265826'
+Parent_no = '+918096257484'
+Github Link = 'https://github.com/SSumithreddy'
+College Name = 'SATHYABAMA INSTITUTE OF SCIENCE AND TECHNOLOGY'
+USN Number = '21s115951'
+Qualification = 'UG (Bachelor Degrees)'
+Department = 'B.Tech - Computer Science (CSE, Cybersecurity, AI & ML, IoT, Cloud Computing, Data Analytics, Data Science)'
+Pass out Year = '2025'
+Graduation Percentage = '87%'
+Arrears = No
+No of Arrears = 0
+10th Pass Year = '2019'
+10th Percentage = '79%''
+12th Pass Year = '2021'
+12th Percentage = '85%'
+Skills = ('Python, Flask, HTML, CSS, Bootstrap, Javascript, MySQL, Data analysis)
+
+print('___________personal information___________')
+print(f'student ID: {student_ID}')
+print(f'Batch No: {Batch_No}')
+print(f'Email ID: {Email_ID}')
+
+
+
+print('_______________Academic Information____________')
+print(f'Department: {Department}')
+print(f'college Name: {Collage_Name}')
+print(f'Qualification: {Qualification}')
+          
